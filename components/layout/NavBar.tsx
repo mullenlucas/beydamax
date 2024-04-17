68afb508646970d891b05ad317baf551dc334690
Lucas Mullen <mullenlucas@gmail.com>
2024-04-17 12:31:15 -0300

    add layouts and models for auther

--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -1,1 +1,13 @@
 'use client'
+
+import { UserButton } from '@clerk/nextjs';
+
+const NavBar = () => {
+  return (
+    <nav>
+      <UserButton afterSignOutUrl='/' />
+    </nav>
+  )
+}
+
+export default NavBar;
