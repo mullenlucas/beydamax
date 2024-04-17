e430c8f475acc4f51043b4f626d50f781a9ef728
Lucas Mullen <mullenlucas@gmail.com>
2024-04-17 13:07:30 -0300

    add layout for navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,32 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { ClerkProvider } from '@clerk/nextjs'
-import NavBar from "@/components/layout/NavBar";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { ClerkProvider } from '@clerk/nextjs';
+import NavBar from '@/components/layout/NavBar';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Beydamax",
-  description: "Alquiler de productos",
-  icons: {icon: '/logo.svg'}
+	title: 'Beydamax',
+	description: 'Alquiler de productos',
+	icons: { icon: '/logo.svg' },
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <ClerkProvider>
-      <html lang="en" suppressHydrationWarning>
-        <NavBar />
-        <body className={inter.className}>{children}</body>
-      </html>
-    </ClerkProvider>
-  );
+	return (
+		<ClerkProvider>
+			<html lang="en" suppressHydrationWarning>
+				<body className={inter.className}>
+					<main className="flex flex-col min-h-screen bg-secondary">
+						<NavBar />
+						<section className="flex-grow">{children}</section>
+					</main>
+				</body>
+			</html>
+		</ClerkProvider>
+	);
 }
